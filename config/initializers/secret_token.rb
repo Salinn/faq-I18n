0112490283e060a766a77180eb4c2d595b087c06
Tall Paul <pmd6624@rit.edu>
2015-01-23 11:15:03 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FaqI18n::Application.config.secret_key_base = 'f97a56a18b1ad10c7529d086ee88d129d5ef1b3c55f6b09e70464bb57c6aaead28123560e54c20e22c1d95ee057d35300c924acc47072a3fd2b44bb385e860a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FaqI18n::Application.config.session_store :cookie_store, key: '_faq-I18n_session'
