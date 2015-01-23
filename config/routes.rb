df9bad09ef5fe83d64157ae45164176dad4edc04
Tall Paul <pmd6624@rit.edu>
2015-01-23 11:26:56 -0500

    aded faq files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FaqI18n::Application.routes.draw do
+  resources :faqs
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
